1ab0209c43a4788e0970f9ba1d3715c22193d598
saud <saudchougle.sc@gmail.com>
2021-06-30 22:13:31 +0530

    modification env

--- backend/src/shared/services/caching/application/redisApplicationClient.ts
+++ backend/src/shared/services/caching/application/redisApplicationClient.ts
@@ -1,9 +1,12 @@
 import redis from 'redis';
 
-const redisApplicationClient = redis.createClient({ db: 1 });
+const redisApplicationClient = redis.createClient({
+  host: 'redis-19945.c275.us-east-1-4.ec2.cloud.redislabs.com:19945',
+  password: 'GVrFlCdkuvcyFzmh7PptZIHve3NxjeXo',
+});
 
 redisApplicationClient.on('connect', () => {
-    console.log('[Redis application client]: Connected to redis server');
+  console.log('[Redis application client]: Connected to redis server');
 });
 
 export { redisApplicationClient };
